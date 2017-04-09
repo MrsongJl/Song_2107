0ae98af2b6455c0ec45a97499590f79aded5547b
Administrator <Administrator@WIN-10>
2017-04-09 13:23:38 +0800

    增加ef及缓存处理

---
+++ Song_Models/GetModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Song_Models
+{
+
+    public class GetModel
+    {
+        public static Song_2017Entities DB
+        {
+            get
+            {
+                return new Song_2017Entities();
+            }
+        }
+        /// <summary>
+        /// 存放缓存 todo: 暂未编写
+        /// </summary>
+        public static Song_2017Entities DBCache
+        {
+            get
+            {
+                return new Song_2017Entities();
+            }
+        }
+
+        public static Stopwatch timerObj = new Stopwatch();
+        /// <summary>
+        /// 验证执行时间开始
+        /// </summary>
+        public static void TimeStart()
+        {
+            timerObj.Start();
+        }
+        /// <summary>
+        ///  验证执行时间结束
+        /// </summary>
+        /// <returns></returns>
+        public static TimeAs TimeEnd()
+        {
+            TimeAs t = new TimeAs();
+            timerObj.Stop();
+            //Logger.GetInstance().Debug("毫秒数 :" + timerObj.ElapsedMilliseconds);
+            //Logger.GetInstance().Debug("时间数 :" + timerObj.Elapsed);
+            t.Milliseconds = timerObj.ElapsedMilliseconds.ToString();
+            t.Time = timerObj.Elapsed.ToString();
+            timerObj.Reset();
+            return t;
+        }
+
+
+    }
+
+    public class TimeAs
+    {
+        public string roots { get; set; }
+        public string Milliseconds { get; set; }
+        public string Time { get; set; }
+    }
+
+}
---
+++ Song_Public/EFHelpler.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Song_Models;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Song_Public
+{
+
+    /// <summary>
+    /// 实现EF普通操作的封装
+    /// </summary>
+    class EFHelpler<T> where T : class
+    {
+
+        /// <summary>
+        /// 实体新增 EFHelpler<User> helper = new EFHelpler<User>(); helper.add(Userlist.ToArray());
+        /// </summary>
+        /// <param name="model"></param>
+        public void add(params T[] paramList)
+        {
+            foreach (var model in paramList)
+            {
+                GetModel.DB.Entry<T>(model).State = EntityState.Added;
+            }
+            GetModel.DB.SaveChanges();
+        }
+        /// <summary>
+        /// 实体查询 var query = helper.getSearchList(item => item.UserName.Contains("keso"));
+        /// </summary>
+        public IEnumerable<T> getSearchList(Expression<Func<T, bool>> where)
+        {
+            return GetModel.DB.Set<T>().Where(where);
+        }
+        /// <summary>
+        /// 实体分页查询 var queryMulti = helper.getSearchListByPage<int>(item => item.UserName.Contains("FlyElehant"), order => order.PersonID, 2, 1);
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="where"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="pageIndex"></param>
+        /// <returns></returns>
+        public IEnumerable<T> getSearchListByPage<TKey>(Expression<Func<T, bool>> where, Expression<Func<T, TKey>> orderBy, int pageSize, int pageIndex)
+        {
+            return GetModel.DB.Set<T>().Where(where).OrderByDescending(orderBy).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+        }
+        /// <summary>
+        /// 实体删除 helper.getSearchList(item => item.UserName.Contains("keso"));helper.delete(query.ToArray());
+        /// </summary>
+        /// <param name="model"></param>
+        public void delete(params T[] paramList)
+        {
+            foreach (var model in paramList)
+            {
+                GetModel.DB.Entry<T>(model).State = EntityState.Deleted;
+            }
+            GetModel.DB.SaveChanges();
+        }
+        /// <summary>
+        /// 按照条件修改数据 Dictionary<string,object> dic=new Dictionary<string,object>();dic.Add("PersonID",2); dic.Add("UserName","keso");helper.update(item => item.UserName.Contains("keso"), dic);
+        /// </summary>
+        /// <param name="where"></param>
+        /// <param name="dic"></param>
+        public void update(Expression<Func<T, bool>> where, Dictionary<string, object> dic)
+        {
+            IEnumerable<T> result = GetModel.DB.Set<T>().Where(where).ToList(); //找到修改的数据列表【原数据】
+            Type type = typeof(T); //找到数据对象类型
+            List<PropertyInfo> propertyList = type.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).ToList(); //利用反射 修改赋值对象集合【新数据】
+            //遍历结果集 并对比相同列进行赋值
+            foreach (T entity in result)
+            {
+                foreach (PropertyInfo propertyInfo in propertyList)
+                {
+                    string propertyName = propertyInfo.Name;
+                    if (dic.ContainsKey(propertyName))
+                    {
+                        //设置值
+                        propertyInfo.SetValue(entity, dic[propertyName], null);
+                    }
+                }
+            }
+            GetModel.DB.SaveChanges();
+        }
+
+        /// <summary>
+        /// 分页拓展 json
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="select"></param>
+        /// <param name="setting"></param>
+        /// <param name="behavior"></param>
+        /// <returns></returns>
+
+        //public static string SerializeQuery<T>(IQueryable<T> query, Func<T, List<string>> select, GridSettings setting, JsonRequestBehavior behavior)
+        //{
+        //    var json = new JsonResult();
+        //    json.JsonRequestBehavior = behavior;
+        //    //查询条件过滤
+        //    if (setting.IsSearch && setting.Where.rules != null)
+        //    {
+        //        if (setting.Where.groupOp == "AND") // 且的关系，多个查询条件都满足
+        //        {
+        //            foreach (var rule in setting.Where.rules)
+        //            {
+        //                query = query.Where(rule.field, rule.data, rule.op);
+        //            }
+        //        }
+        //        else if (setting.Where.groupOp == "OR") // 或的关系，至少满足其中一个条件
+        //        {
+        //            //var temp = (new List<T>()).AsQueryable();
+        //            IQueryable<T> temp = null;
+        //            foreach (var rule in setting.Where.rules)
+        //            {
+        //                if (temp != null)
+        //                {
+        //                    var t = query.Where(rule.field, rule.data, rule.op);
+        //                    temp = temp.Concat(t);//连接查询条件
+        //                }
+        //                else
+        //                { temp = query.Where(rule.field, rule.data, rule.op); }
+        //            }
+        //            query = temp.Distinct();//去掉重复
+        //        }
+        //    }
+        //    //排序
+        //    query = query.OrderBy(setting.SortColumn, setting.SortOrder);
+
+        //    //统计行数
+        //    var totalCount = query.Count();
+
+        //    //分页
+        //    var data = query.Skip((setting.PageIndex - 1) * setting.PageSize).Take(setting.PageSize);
+
+        //    //格式转化
+        //    json.Data = new
+        //    {
+        //        page = setting.PageIndex,
+        //        records = totalCount,
+        //        total = (int)Math.Ceiling((double)totalCount / setting.PageSize),
+        //        rows = data.ToList().Select((d, id) => new { id, cell = select(d) }).ToArray()
+        //    };
+        //    return json;
+        //}
+    }
+}
